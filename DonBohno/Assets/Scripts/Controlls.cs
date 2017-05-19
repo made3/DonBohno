1550d018ebcb80290e62be16fc183e1e6e26e154
Martin Beyrle <martin.beyrle@web.de>
2017-05-19 23:43:38 +0200

    Merge branch 'master' of https://github.com/made3/DonBohno

---
+++ DonBohno/Assets/Scripts/Controlls.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlls : MonoBehaviour {
+
+    public string playerID;
+    public GameObject thePlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Move Input
+            var x = Input.GetAxis("Horizontal_P" + playerID) * Time.deltaTime * 3.0f;
+            var z = Input.GetAxis("Vertical_P" + playerID) * Time.deltaTime * 3.0f;
+
+        //shoot:
+        
+            var xr = -Input.GetAxis("HorizontalRight_P" + playerID);
+            var zr = Input.GetAxis("VerticalRight_P" + playerID);
+        if(xr != 0 || zr !=0)
+        thePlayer.transform.LookAt(new Vector3(xr,0,zr));
+        else
+            thePlayer.transform.LookAt(new Vector3(x, 0, z));
+
+        //Debug.Log("x:" + x); Debug.Log("y:" + z);
+        Debug.Log("x:"+ xr); Debug.Log("y:" + zr);
+        this.transform.Translate(x, 0, z);
+        //Direction.transform.Rotate(0, PlayerRotX * 2.0f, 0); ?
+
+    }
+}
