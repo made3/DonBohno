3098ee5ff42cc371890ada8828e0d6f42e7deadd
Z0MBR33 <pascal.plewa97@hotmail.de>
2017-05-20 20:02:35 +0200

    Camera Script

---
+++ DonBohno/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject Camera;
+
+    float p1x;
+    float p1y;
+    float p2x;
+    float p2y;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p1x = player1.transform.position.x;
+        p1y = player1.transform.position.z;
+        p2x = player2.transform.position.x;
+        p2y = player2.transform.position.z;
+        
+
+
+        Camera.transform.position = new Vector3((p1x+p2x)/2,0,(p1y+p2y)/2);
+    }
+}
