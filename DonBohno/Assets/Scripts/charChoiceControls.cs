4684c3adf9f3f01515d45fbbba40ec707804431b
Martin Beyrle <martin.beyrle@web.de>
2017-05-21 16:22:18 +0200

    Character Selection added

---
+++ DonBohno/Assets/Scripts/charChoiceControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charChoiceControls : MonoBehaviour {
+
+    public string playerID;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*
+        float xRot = -Input.GetAxis("Horizontal_P" + playerID);
+        float zRot = Input.GetAxis("Vertical_P" + playerID);
+        if(xRot > 0 && zRot == 0)
+        {
+            Debug.Log("nach links");
+        }
+        else if (xRot < 0 && zRot == 0)
+        {
+            Debug.Log("nach rechts");
+        }*/
+    }
+}
